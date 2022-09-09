9f87ad705ab94f02502ca5e9a29a46426f06bfe7
zhangxiaosheng <z1178858918@qq.com>
2022-09-09 11:28:50 +0800

    Initial commit

---
+++ src/main/kotlin/com/example/kotlin/KotlinTestApplication.kt
@@ -0,0 +1,25 @@
+package com.example.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class KotlinTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinTestApplication>(*args)
+}
+
+data class Message(val id: String?, val text: String)
+
+@RestController
+class MessageResource {
+    @GetMapping
+    fun index(): List<Message> = listOf(
+        Message("1", "Hello!"),
+        Message("2", "Bonjour!"),
+        Message("3", "Privet!"),
+    )
+}
