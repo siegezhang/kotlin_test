7b55fa7879df4e5adddabd2d80b579193a436c45
zhangxiaosheng <z1178858918@qq.com>
2022-09-09 13:04:50 +0800

    commit

--- src/main/kotlin/com/example/kotlin/KotlinTestApplication.kt
+++ src/main/kotlin/com/example/kotlin/KotlinTestApplication.kt
@@ -2,7 +2,12 @@ package com.example.kotlin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jdbc.repository.query.Query
+import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.repository.CrudRepository
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
@@ -12,14 +17,23 @@ fun main(args: Array<String>) {
     runApplication<KotlinTestApplication>(*args)
 }
 
+@Table("messages")
 data class Message(val id: String?, val text: String)
 
+
 @RestController
-class MessageResource {
+class MessageResource(val service: MessageService) {
     @GetMapping
-    fun index(): List<Message> = listOf(
-        Message("1", "Hello!"),
-        Message("2", "Bonjour!"),
-        Message("3", "Privet!"),
-    )
+    fun index(): List<Message> = service.findMessages()
+
+    @PostMapping
+    fun post(@RequestBody message: Message) {
+        service.post(message)
+    }
+}
+
+interface MessageRepository : CrudRepository<Message, String> {
+
+    @Query("select * from messages")
+    fun findMessages(): List<Message>
 }
---
+++ src/main/kotlin/com/example/kotlin/MessageService.kt
@@ -0,0 +1,14 @@
+package com.example.kotlin
+
+import org.springframework.stereotype.Service
+
+
+@Service
+class MessageService(val db: MessageRepository) {
+
+    fun findMessages(): List<Message> = db.findMessages()
+
+    fun post(message: Message){
+        db.save(message)
+    }
+}
