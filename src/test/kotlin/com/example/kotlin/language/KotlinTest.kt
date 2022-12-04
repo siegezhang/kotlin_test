fe38804ddd05ce328485cba1b0a664dc464a8563
zhangxiaosheng <z1178858918@qq.com>
2022-12-04 22:47:02 +0800

    commit

--- src/test/kotlin/com/example/kotlin/language/KotlinTest.kt
+++ src/test/kotlin/com/example/kotlin/language/KotlinTest.kt
@@ -2,6 +2,14 @@ package com.example.kotlin.language
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+
+/*
+import kotlinx.coroutines.*
+*/
+
 
 class KotlinTest {
     @Test
@@ -55,4 +63,108 @@ class KotlinTest {
         val printer = CustomPrinter(DefaultPrinter())
         printer.print() //DefaultPrinter print
     }
+
+    @OptIn(ExperimentalContracts::class)
+    fun calledOneTimeOnly(run: () -> Unit) {
+        // do something
+        contract {
+            callsInPlace(run, InvocationKind.EXACTLY_ONCE)
+        }
+        run()
+    }
+
+    fun initValue() {
+        val intValue: Int
+        calledOneTimeOnly {
+            // Does not compile:
+            // Captured values initialization is forbidden due to possible reassignment.
+            intValue = 1
+        }
+//        calledOneTimeOnly {
+//            intValue = 2
+//        }
+    }
+
+    fun getString(): String? {
+        return null
+    }
+
+//    @ExperimentalContracts
+//    fun Any?.isValidString(): Boolean {
+//        contract {
+//            returns(true) implies (this@getString is String)
+//        }
+//        return this != null && this is String && this.length > 0
+//    }
+//
+//
+//    @ExperimentalContracts
+//    fun testString() {
+//        val test = getString()
+//
+//        if (test.isValidString()) {
+//            // Compiles.
+//            val result: String = test
+//        }
+//    }
+
+    /**
+     * 字符串扩展函数判空，常规方式
+     * @receiver String?  接收类型
+     * @return Boolean    是否为空
+     */
+//    fun String?.isNullOrEmptyWithoutContract(): Boolean {
+//        return this == null || this.isEmpty()
+//    }
+//
+//    /**
+//     * 问题示例1 使用自定义函数判空，编译器无感知
+//     * @param name String? 传入的姓名字符串
+//     */
+//    private fun problemNull(name: String?) {
+//        // 用常规方式的自定义扩展函数对局部变量判空
+//        if (!name.isNullOrEmptyWithoutContract()) {
+//            //name.length报错，自定义扩展函数中的判空逻辑未同步到编译器 Only safe (?.) or non-null asserted (!!.) calls are allowed on a nullable receiver of type String?
+//            println("name:$name,length:${name.length}")
+//        }
+//    }
+
+    /**
+     * 字符串扩展函数判空,contract方式
+     * @receiver String?  接收类型
+     * @return Boolean    是否为空
+     */
+    @OptIn(ExperimentalContracts::class)
+    fun String?.isNullOrEmptyWithContract(): Boolean {
+        contract {
+            returns(false) implies (this@isNullOrEmptyWithContract != null)
+        }
+        return this == null || this.isEmpty()
+    }
+
+    /**
+     * 解决问题1 自定义函数判空后结果同步编译器
+     * @param name String? 传入的姓名字符串
+     */
+    fun fixProblemNull(name: String?) {
+        // 用contract方式的自定义扩展函数对局部变量判空
+        if (!name.isNullOrEmptyWithContract()) {
+            //运行正常
+            // println("name:$name,length:${name.length}")
+        }
+    }
+
+    fun main() {
+//        runBlocking {
+//            println("start")
+//            launch {
+//                println("async work started...")
+//                delay(200)
+//                println("async work done!")
+//            }
+//            println("end")
+//        }
+    }
+
 }
+
---
+++ src/test/kotlin/com/example/kotlin/language/collection/CollectionTest.kt
@@ -0,0 +1,109 @@
+package com.example.kotlin.language.collection;
+
+import org.junit.jupiter.api.Test;
+
+public class CollectionTest {
+    @Test
+    fun testCollection() {
+        val numbers = mutableListOf("one", "two", "three", "four")
+        numbers.add("five")   // this is OK
+        println(numbers)
+        //numbers = mutableListOf("six", "seven")      // compilation error
+    }
+
+    @Test
+    fun testCollection1() {
+        val stringList = listOf("one", "two", "one")
+        printAll(stringList)
+
+        val stringSet = setOf("one", "two", "three")
+        printAll(stringSet)
+    }
+
+    @Test
+    fun testCollection2() {
+        val words = "A long time ago in a galaxy far far away".split(" ")
+        val shortWords = mutableListOf<String>()
+        words.getShortWordsTo(shortWords, 3)
+        println(shortWords)
+    }
+
+    @Test
+    fun testCollection3() {
+        val numbers = listOf("one", "two", "three", "four")
+        println("Number of elements: ${numbers.size}")
+        println("Third element: ${numbers.get(2)}")
+        println("Fourth element: ${numbers[3]}")
+        println("Index of element \"two\" ${numbers.indexOf("two")}")
+    }
+
+    data class Person(var name: String, var age: Int)
+
+    @Test
+    fun testCollection4() {
+        val bob = Person("Bob", 31)
+        val people = listOf(Person("Adam", 20), bob, bob)
+        val people2 = listOf(Person("Adam", 20), Person("Bob", 31), bob)
+        println(people == people2)
+        bob.age = 32
+        println(people == people2)
+    }
+
+    @Test
+    fun testCollection5() {
+        val numbers = setOf(1, 2, 3, 4)
+        println("Number of elements: ${numbers.size}")
+        if (numbers.contains(1)) println("1 is in the set")
+
+        val numbersBackwards = setOf(4, 3, 2, 1)
+        println("The sets are equal: ${numbers == numbersBackwards}")
+    }
+
+    @Test
+    fun testCollection6() {
+        val numbers = setOf(1, 2, 3, 4)  // LinkedHashSet is the default implementation
+        val numbersBackwards = setOf(4, 3, 2, 1)
+
+        println(numbers.first() == numbersBackwards.first())
+        println(numbers.first() == numbersBackwards.last())
+    }
+
+    @Test
+    fun testCollection7() {
+        val numbersMap = mapOf("key1" to 1, "key2" to 2, "key3" to 3, "key4" to 1)
+
+        println("All keys: ${numbersMap.keys}")
+        println("All values: ${numbersMap.values}")
+        if ("key2" in numbersMap) println("Value by key \"key2\": ${numbersMap["key2"]}")
+        if (1 in numbersMap.values) println("The value 1 is in the map")
+        if (numbersMap.containsValue(1)) println("The value 1 is in the map") // same as previous
+    }
+
+    @Test
+    fun testCollection8() {
+        val numbersMap = mapOf("key1" to 1, "key2" to 2, "key3" to 3, "key4" to 1)
+        val anotherMap = mapOf("key2" to 2, "key1" to 1, "key4" to 1, "key3" to 3)
+
+        println("The maps are equal: ${numbersMap == anotherMap}")
+    }
+
+    @Test
+    fun testCollection9() {
+        val numbersMap = mutableMapOf("one" to 1, "two" to 2)
+        numbersMap.put("three", 3)
+        numbersMap["one"] = 11
+        println(numbersMap)
+    }
+
+    fun printAll(strings: Collection<String>) {
+        for (t in strings) print("$t ")
+        println()
+    }
+
+    fun List<String>.getShortWordsTo(shortWords: MutableList<String>, maxLength: Int) {
+        this.filterTo(shortWords) { it.length <= maxLength }
+        // throwing away the articles
+        val articles = setOf("a", "A", "an", "An", "the", "The")
+        shortWords -= articles
+    }
+}
